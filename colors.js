cc4cf253d399fe1ed4567d5194d051ef91edaa14
Dan Cramer <dan@safeandreliablecare.com>
2023-03-29 11:22:46 -0600

    updating colors

--- colors.js
+++ colors.js
@@ -1,12 +1,12 @@
 module.exports = [
-	'#01888C', // teal
-  '#FC7500', // bright orange
-  '#034F5D', // dark teal
-  '#F73F01', // orangered
-  '#FC1960', // magenta
-  '#C7144C', // raspberry
-  '#F3C100', // goldenrod
-  '#1598F2', // lightning blue
-  '#2465E1', // sail blue
-  '#F19E02', // gold
+	'#F98300',
+'#04435E',
+'#53AFB1',
+'#FFC93C',
+'#BAD9DA',
+'#EE4445',
+'#6E1244',
+'#F8B9B9',
+'#F0E9E7',
+'#C195B1',
 ]
